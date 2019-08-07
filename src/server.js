2edc73b48d7d76c88859741311fc2601959b3955
Hiago Souza <hiagofss98@gmail.com>
2019-08-06 23:14:53 -0300

    Create Model Devs

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = require('./routes');
+const mongoose = require('mongoose');
 
 const server = express();
+
+mongoose.connect(
+  'mongodb+srv://omnistack:omnistack@cluster0-bmqmo.mongodb.net/omnistack8?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+);
 
 server.use(express.json());
 
