b000796458ea99a7e04169000a3139fd55344901
Hiago Souza <hiagofss98@gmail.com>
2019-08-08 18:36:51 -0300

    Create Likes and Dislikes Controller

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,12 @@ const { Router } = require('express');
 const routes = Router();
 
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 routes.post('/devs', DevController.store);
+
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
